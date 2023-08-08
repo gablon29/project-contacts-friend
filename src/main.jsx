0aa2593b1dee3b27a8ef9453b5ef62e2c8e5e80f
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-08 18:35:19 -0300

    incluyendo componente de contacts.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Root from './routes/root.jsx'
+import Root, { loader as rootLoader } from './routes/root.jsx'
 import ErrorPage from './Error-page'
+import Contact from './routes/contact'
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root/>,
     errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
   },
 ])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
