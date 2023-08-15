f4d6336b96c2f622af68e13d9ef1b354de7e14e3
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-15 18:14:41 -0300

    incluyendo funcion para agregar contactos

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Root, 
-{ loader as rootLoader,
-  action as rootAction, } from './routes/root.jsx'
+import Root,
+{
+  loader as rootLoader,
+  action as rootAction,
+} from './routes/root.jsx'
 import ErrorPage from './Error-page'
-import Contact from './routes/contact'
+import Contact, { loader as contactLoader } from './routes/contact'
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
-    errorElement: <ErrorPage/>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
-        element: <Contact />
+        element: <Contact />,
+        loader: contactLoader,
       }
     ]
   },
@@ -28,6 +31,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={ router }/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
