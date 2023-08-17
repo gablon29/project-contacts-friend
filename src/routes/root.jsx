c4c25eb77efaf14e82af5392a7b93ef17fe433e4
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-17 19:24:57 -0300

    incluyendo onchange en el formulario de busqueda

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,8 +6,10 @@ import {
   Form,
   redirect,
   useNavigation,
+  useSubmit,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export async function action() {
@@ -19,12 +21,17 @@ export async function loader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    document.getElementById("q").value = q
+  }, [q])
 
   return (
     <>
@@ -38,6 +45,10 @@ export default function Root() {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
+              onChange={(evento => {
+                submit(evento.currentTarget.form);
+              })}
             />
             <div
               id="search-spinner"
