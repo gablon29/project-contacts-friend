5a392180f072dcba2cdce41be5483abd1cfc2836
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-18 16:45:15 -0300

    agregando respuesta 404

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -2,7 +2,13 @@ import { Form, useLoaderData, useFetcher, } from "react-router-dom";
 import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
-  const contact = await getContact(params.contactId)
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
 export async function action({ request, params }) {
@@ -77,6 +83,9 @@ export default function Contact() {
 function Favorite({ contact }) {
   const fetcher = useFetcher();
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
   return (
     <fetcher.Form method="post">
       <button
